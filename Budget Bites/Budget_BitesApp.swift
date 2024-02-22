f7b87d6b0e593b6fd2751401c73416e7adc268af
nelsosp <nelsonsp@mail.gvsu.edu>
2024-02-22 00:14:46 -0500

    Initial Commit

---
+++ Budget Bites/Budget_BitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budget_BitesApp.swift
+//  Budget Bites
+//
+//  Created by Spencer Nelson on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Budget_BitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Budget Bites/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Budget Bites
+//
+//  Created by Spencer Nelson on 2/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
