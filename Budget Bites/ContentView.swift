aa62391c08f761acd815ec745944f1f7d5c2fbf7
nelsosp <nelsonsp@mail.gvsu.edu>
2024-03-02 17:37:16 -0500

    Ensuring Additions of Files

--- Budget Bites/ContentView.swift
+++ Budget Bites/ContentView.swift
@@ -1,24 +1,116 @@
-//
-//  ContentView.swift
-//  Budget Bites
-//
-//  Created by Spencer Nelson on 2/22/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                // Header
+//                Text("Budget Bites")
+//                    .font(.largeTitle)
+//                    .fontWeight(.bold)
+//                    .padding()
+                
+                // Main content
+                Spacer()
+                VStack {
+                    Text("Budget")
+                        .font(.largeTitle)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.white)
+                    Text("Bites")
+                        .font(.largeTitle)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.white)
+                    Text("For the people, By the people")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                    
+                    NavigationLink(destination: Text("Recipe Finder")) {
+                        Text("Find Recipes")
+                            .fontWeight(.bold)
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                }
+
+                .padding()
+                .background(Color.black.opacity(0.5))
+                .cornerRadius(10)
+                .padding()
+                
+                Spacer()
+                
+                // Navigation links
+                VStack {
+                    NavigationLink(destination: Text("Recipe Finder")) {
+                        Text("Recipe Finder")
+                    }
+                    NavigationLink(destination: Text("About Us")) {
+                        Text("About Us")
+                    }
+                    NavigationLink(destination: SignInEmailView()) {
+                        Text("Login")
+                    }
+                    NavigationLink(destination: Text("Contact")) {
+                        Text("Contact")
+                    }
+                }
+                .padding(.top, 0)
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+                
+                Spacer()
+            }
+            .background(
+                VStack {
+                    Image("food")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(maxWidth: .infinity)
+                        .frame(height: UIScreen.main.bounds.height * 0.68) // 3/4 of the screen height
+                        .clipped()
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Image("fb")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                        Image("ig")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                        Image("tw")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                        Spacer()
+                    }
+                    .padding(.bottom, 50)
+                }
+                .background(Color.black)
+                .edgesIgnoringSafeArea(.all)
+            )
+            .navigationBarItems(trailing:
+                Menu {
+                    NavigationLink(destination: Text("Recipe Finder")) {
+                        Label("Recipe Finder", systemImage: "magnifyingglass")
+                    }
+                } label: {
+                    Image(systemName: "line.horizontal.3")
+                        .font(.title)
+                        .padding()
+                }
+            )
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
